fe88d166dd710eef3bf38e12b1ed62781d88ac72
Roland Green <roland.green@highpowerdata.com>
2023-07-17 10:38:32 -0700

    updated pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4227135ec6e1c842"; }
+/******/ 		__webpack_require__.h = function() { return "a031c4b5f5c18e7c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.47a3221e3094c5f6.hot-update.js
+++ .next/static/webpack/webpack.47a3221e3094c5f6.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "db05855d9929b3d5"; }
+/******/ 	__webpack_require__.h = function() { return "42f7053d9511a32e"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
