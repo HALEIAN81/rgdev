8040a4746d55d2acfdd126cfaa2efee8b59d5fb0
Roland Green <roland.green@highpowerdata.com>
2023-07-18 06:10:54 -0700

    updated pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -34,14 +34,14 @@ function MyApp({ Component, pageProps }) {
           />
           <meta name="description" content="description" />
           <meta name="robots" content="noindex, nofollow" />
-          <meta name="keywords" content="React Template, Portfolio Template" />
-          <meta name="author" content="Nuclear Themes" />
+          <meta name="keywords" content="RolandGreen.Dev, Portfolio" />
+          <meta name="author" content="RolandGreen.Dev" />
           <meta name="theme-color" content="#72E2AE" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
           <link rel="apple-touch-icon" href="/icon-192x192.png" />
-          <title>Bieber - React Personal Portfolio Template</title>
+          <title>Welcome to RolandGreen.Dev - Portfolio</title>
         </Head>
         <ParallaxProvider>
           <Hydrate state={pageProps.dehydratedState}>
