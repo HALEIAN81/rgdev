2ac66ae6cba5c6ae81885de2ce0d0864f55df645
Roland Green <roland.green@highpowerdata.com>
2023-07-24 16:26:33 -0700

    page updates, css changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a031c4b5f5c18e7c"; }
+/******/ 		__webpack_require__.h = function() { return "43837098ca091e22"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
